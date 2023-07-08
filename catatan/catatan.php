599569bd99bf45a5e86cea1e8dd6c5e170895854
DillonVantra <dillonleon82@gmail.com>
2023-07-08 07:34:14 +0700

    init

---
+++ catatan/catatan.php
@@ -0,0 +1,20 @@
+<?php
+include "header.php";
+?>
+
+<html>
+    <body>
+        <table border="1" align="center" width="50%">
+            <td>
+                <a>Urutkan Berdasarkan</a>
+                <select name="" id="">
+                    <option value="">Tanggal</option>
+                    <option value="">Waktu</option>
+                    <option value="">Lokasi</option>
+                    <option value="">Suhu</option>
+                </select>
+                <input type="submit" value="urutkan">
+            </td>
+        </table>
+    </body>
+</html>
