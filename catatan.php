af1a9d6c8642afb2bcb0db1011cd5cdd271cb6a3
DillonVantra <dillonleon82@gmail.com>
2023-07-08 09:26:41 +0700

    selesai sini aja

---
+++ catatan.php
@@ -0,0 +1,23 @@
+<?php
+include "header.php";
+include 'koneksi.php';
+?>
+
+<html>
+
+<body>
+  <table border="1" align="center" width="50%">
+    <td>
+      <a>Urutkan Berdasarkan</a>
+      <select name="" id="">
+        <option value="">Tanggal</option>
+        <option value="">Waktu</option>
+        <option value="">Lokasi</option>
+        <option value="">Suhu</option>
+      </select>
+      <input type="submit" value="urutkan" />
+    </td>
+  </table>
+</body>
+
+</html>
--- login.php
+++ login.php
@@ -1,52 +1,61 @@
 <?php
- if(isset($_POST['daftar']))
- {
-    $nik = $_POST['nik'];
-    $nama = $_POST['nama'];
-    $text = $nik . "," . $nama . "\n";
-    $fp = fopen('config.txt', 'a+');
+include 'koneksi.php';
 
-    if(fwrite($fp, $text)){
-        echo '<script>alert("Anda Berhasil Mendaftar!"); </script>';
-    }
- }
- else if(isset($_POST['masuk']))
- {
-    $data = file_get_contents("config.txt");
-    $contens = explode("\n", $data);
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+	if (isset($_POST["daftar"])) {
+		$nik = $_POST["nik"];
+		$nama = $_POST["nama"];
 
-    foreach($contens as $values){
-        $login = explode(",", $values);
-        $nik = $login[0];
-        $nama = $login[1];
+		$query = "INSERT INTO data1 (nik, nama) VALUES ('$nik', '$nama')";
 
-        if($nik == $_POST['nik'] && $nama == $_POST['nama']){
-            session_start();
-            $_SESSION['username'] = $nama;
-            header('location: home.php');
-        }else{
-            echo '<script>alert("Nik atau Nama Anda Salah!.");</script>';
-        }
-    }
- }
+		$result = mysqli_query($koneksi, $query);
+
+		if ($result) {
+			echo "Pengguna baru berhasil ditambahkan.";
+		} else {
+			echo "Terjadi kesalahan dalam menambahkan pengguna baru.";
+		}
+	} elseif (isset($_POST["masuk"])) {
+		$nik = $_POST["nik"];
+		$nama = $_POST["nama"];
+
+		$query = "SELECT * FROM data1 WHERE nik = '$nik'";
+		$result = mysqli_query($koneksi, $query);
+
+		if (mysqli_num_rows($result) > 0) {
+			$row = mysqli_fetch_assoc($result);
+			$hashedNik = $row['nik'];
+
+			if (password_verify($nik, $hashedNik)) {
+				session_start();
+				$_SESSION['username'] = $nama;
+				header("Location: home.php");
+				exit();
+			} else {
+				echo '<script>alert("NIK atau Nama Anda Salah! Silahkan isi kembali.");</script>';
+			}
+		}
+	}
+}
 ?>
-<html>
-    <form action="" method="POST">
-        <br><br><br><br><br><br><br>
-        <table align="center">
-            <tr>
-                 <td><input type="text" name="nik" placeholder="NIK"></td>
-            </tr>
-             <tr>
-                <td><input type="text" name="nama" placeholder="Nama Lengkap"></td>
-             </tr>
-             <tr>
-                <td>
-                    <input type="submit" name="daftar" value="Saya Pengguna Baru">
-                    <input type="submit" name="masuk" value="Masuk">
-                </td>
 
-             </tr>
-        </table>
-    </form>
+<html>
+<form action="" method="POST">
+	<br><br><br><br><br><br><br>
+	<table align="center">
+		<tr>
+			<td><input type="text" name="nik" placeholder="NIK"></td>
+		</tr>
+		<tr>
+			<td><input type="text" name="nama" placeholder="Nama Lengkap"></td>
+		</tr>
+		<tr>
+			<td>
+				<input type="submit" name="daftar" value="Daftar">
+				<input type="submit" name="masuk" value="Masuk">
+			</td>
+		</tr>
+	</table>
+</form>
+
 </html>
